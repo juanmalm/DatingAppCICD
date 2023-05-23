35d4d9c596ed28bae49d84e43ab858c450919ac1
Juanmalm <juanmalm86@gmail.com>
2023-05-23 23:39:57 +0200

    Quitar codigo duplicado

--- API/Interfaces/ITokenService.cs
+++ API/Interfaces/ITokenService.cs
@@ -4,7 +4,6 @@ namespace API.Interfaces
 {
     public interface ITokenService
     {
-        Task<string> CreateToken01(AppUser user);
-        Task<string> CreateToken02(AppUser user);
+        Task<string> CreateToken(AppUser user);
     }
 }
