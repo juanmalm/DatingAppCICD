11fa46c92717c5cc03ee4f1f0425423990d3970a
Juanmalm <juanmalm86@gmail.com>
2023-06-24 00:41:03 +0200

    Terraform clave publica

--- .azpipelines/terraform/despliegue.tf
+++ .azpipelines/terraform/despliegue.tf
@@ -97,7 +97,7 @@ resource "azurerm_linux_virtual_machine" "main" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("id_rsa.pub")
   }
 
   os_disk {
