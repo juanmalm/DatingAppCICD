dc9ca2164402c96893037592a6a473fa904047c5
Juanmalm <juanmalm86@gmail.com>
2023-06-27 07:50:32 +0200

    Test aceptacion

--- API.AcceptanceTests/RegisterTests.cs
+++ API.AcceptanceTests/RegisterTests.cs
@@ -8,10 +8,10 @@ namespace API.AcceptanceTests
     [TestClass]
     public class RegisterTests
     {
-        public TestContext TestContext { get; set; }
+        public TestContext? TestContext { get; set; }
 
-        private IWebDriver driver;
-        private string appURL;
+        private IWebDriver? driver;
+        private string? appURL;
 
         public RegisterTests()
         {
@@ -21,9 +21,9 @@ namespace API.AcceptanceTests
         [TestCategory("Chrome")]
         public void RegisterButtonOpensSignUpForm()
         {
-            driver.Navigate().GoToUrl(appURL + "/");
-            driver.FindElement(By.XPath("//button[text()=\"Register\"]")).Click();
-            Assert.IsTrue(driver.FindElement(By.XPath("//h2[text()=\"Sign up\"]")) != null, "Verified 'Sign up' content");
+            driver?.Navigate().GoToUrl(appURL + "/");
+            driver?.FindElement(By.XPath("//button[text()=\"Register\"]")).Click();
+            Assert.IsTrue(driver?.FindElement(By.XPath("//h2[text()=\"Sign up\"]")) != null, "Verified 'Sign up' content");
         }
 
         [TestInitialize()]
@@ -31,13 +31,16 @@ namespace API.AcceptanceTests
         {
             var appIp = Environment.GetEnvironmentVariable("APP_IP") ?? "127.0.0.1";
             appURL = $"http://{appIp}:8080/";
-            driver = new ChromeDriver();
+
+            var options = new ChromeOptions();
+            options.AddArgument("no-sandbox");
+            driver = new ChromeDriver(options);
         }
 
         [TestCleanup()]
         public void MyTestCleanup()
         {
-            driver.Quit();
+            driver?.Quit();
         }
     }
 }
