f91d65ea4f09f9ec69df88ab3b41a8f2bbf9f1d6
Juanmalm <juanmalm86@gmail.com>
2023-06-29 00:34:54 +0200

    Set IP on environment.prod.ts

--- .azpipelines/terraform/despliegue.tf
+++ .azpipelines/terraform/despliegue.tf
@@ -133,7 +133,7 @@ resource "azurerm_linux_virtual_machine" "main" {
       "[[ ${var.entorno} != \"prod\" ]] || { exit 0; }",
       "cd /app",
       "cp appsettings.json API/appsettings.json",
-      "cd client\src\environments",
+      "cd client/src/environments",
       "sed -i 's/localhost/${azurerm_public_ip.public_ip.ip_address}/' environment.prod.ts",
       "sudo systemctl restart datingapp.service"
     ]
