e9be25623ef079248d5881809cc2163c3af7b188
Juanmalm <juanmalm86@gmail.com>
2023-07-03 23:51:20 +0200

    Comentarios en install-app.sh

--- .azpipelines/packer/scripts/install-app.sh
+++ .azpipelines/packer/scripts/install-app.sh
@@ -2,18 +2,24 @@
 sudo apt-get install -y unzip
 sudo mkdir /app
 
+# Extracción de los artefactos
 sudo unzip /tmp/API.zip -d /app/API
 sudo unzip /tmp/cliente.zip -d /app/cliente
+
+# Copia del appsettings.json de producción
 sudo cp /tmp/appsettings.json /app/API/appsettings.json
 
+# Permisos de ejecución para el API
 sudo chmod 777 /app/API/API
 
+# Establecer la aplicación como servicio del sistema
 sudo cp /tmp/start-app.sh /app/start-app.sh
 sudo chmod +x /app/start-app.sh
 sudo cp /tmp/datingapp.service /lib/systemd/system/datingapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable datingapp.service
 
+# Abrir puertos necesarios
 echo "y" | sudo ufw enable
 sudo ufw allow 22
 sudo ufw allow 8080
